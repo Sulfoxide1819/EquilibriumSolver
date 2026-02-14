b322322fee89a401e0cb71ba540881b5c596b8b6
Korsakov Vladislav <KorsakovVDmechanics@yandex.ru>
2026-02-14 03:19:30 +0300

    add electronic statsums

--- include/thermo/statSum.hpp
+++ include/thermo/statSum.hpp
@@ -5,6 +5,6 @@ public:
   static double translational(const Component& comp, double T);
   static double rotational(const Component& comp, double T);
   static double vibrational(const Component& comp, double T);
-  //static double electronic(const Component& comp, double T);
+  static double electronic(const Component& comp, double T);
   static double total(const Component& comp, double T);
 };
--- src/thermo/statSum.cpp
+++ src/thermo/statSum.cpp
@@ -4,8 +4,6 @@
 #include "core/constants.hpp"
 #include "core/types.hpp"
 
-#include <iostream> //delete later
-
 using std::numbers::pi;
 double StatSum::translational (const Component& comp, double T) {
   double base = 2 * pi * K * T / (h * h) * comp.molar_mass / NA; 
@@ -22,7 +20,19 @@ double StatSum::vibrational(const Component& comp, double T) {
   double denom = 1 - std::exp(-h * c * comp.vibrational_freq / (K * T));
   return (denom > 0) ? 1/denom : 1.0;
 }
+
+double StatSum::electronic(const Component& comp, double T){
+  if(!comp.is_atomic){ return 1.0; }
+  double Z_el = 0;
+  for(const auto& level : comp.energy_levels){
+    double to_exp_term = -h * c * level.energy / (K * T);
+    Z_el += level.degeneracy * std::exp(to_exp_term);
+  }
+  return Z_el;
+}
+
+
 double StatSum::total(const Component& comp, double T) {
   double exp_term = std::exp(comp.dissociation_energy / (K * T * NA));
-  return translational(comp, T) * rotational(comp, T) * vibrational(comp, T) * exp_term;
+  return translational(comp, T) * rotational(comp, T) * vibrational(comp, T) * electronic(comp, T) * exp_term;
 }
